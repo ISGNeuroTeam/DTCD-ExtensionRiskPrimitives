f6c5674b9b5e57e68f74bed9ed4c6f2de7411fb3
sbelikov98 <sbelikov@isgneuro.com>
2022-12-23 19:32:26 +0300

    Added new ExportNode

--- DTCD-ExtensionRiskPrimitives/src/primitives/index.js
+++ DTCD-ExtensionRiskPrimitives/src/primitives/index.js
@@ -2,5 +2,6 @@ import CRLNodes from './RichLabelNodes/CRLNodes';
 import URLNodes from './RichLabelNodes/URLNodes';
 import SRLNodes from './RichLabelNodes/SRLNodes';
 import TRLNodes from './RichLabelNodes/TRLNodes';
+import ExportNode from './ExportNode/ExportNode';
 
-export default [...CRLNodes, ...URLNodes, ...SRLNodes, ...TRLNodes];
+export default [...CRLNodes, ...URLNodes, ...SRLNodes, ...TRLNodes, ExportNode];
